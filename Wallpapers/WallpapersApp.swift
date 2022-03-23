6c81b3d25dcf644211ca01f4f73e77fcd6ce8aa3
AlessianAU <alessia@alessian.org>
2022-03-23 14:08:33 +1100

    Initial Commit

---
+++ Wallpapers/WallpapersApp.swift
@@ -0,0 +1,16 @@
+//  WallpapersApp.swift
+//  Wallpapers
+//
+//  Created by Alessia on 23/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct WallpapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
